ae46462a36fabfe3a6d7444f8240235a5ed1d3e9
Maximilian Gravemeyer <m.gravemeyer@icloud.com>
2020-11-14 22:11:18 +0100

    added media quality model with deccoder

---
+++ Segler/Models/MediaQualityModel.swift
@@ -0,0 +1,9 @@
+//
+//  MediaQualityModel.swift
+//  Segler
+//
+//  Created by Maximilian Gravemeyer on 14.11.20.
+//  Copyright © 2020 Maximilian Gravemeyer. All rights reserved.
+//
+
+import Foundation
