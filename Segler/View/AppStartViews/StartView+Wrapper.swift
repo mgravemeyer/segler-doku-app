0d6e96a3abf57552d3725917792ce6f299b6b2c8
Maximilian Gravemeyer <m.gravemeyer@icloud.com>
2020-11-26 16:11:27 +0100

    version changed to 1.5.4

--- Segler/View/AppStartViews/StartView+Wrapper.swift
+++ Segler/View/AppStartViews/StartView+Wrapper.swift
@@ -1,25 +1,30 @@
 import SwiftUI
 
-struct AppStart: View {
+struct StartView: View {
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         UINavigationBar.appearance().barTintColor = UIColor(red: 200/255, green: 0/255, blue: 0/255, alpha: 1)
-                //Use this if NavigationBarTitle is with displayMode = .inline
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
     }
     
     let seglerColor: Color = Color(red: 200/255, green: 0/255, blue: 0/255)
     
     //CREATING ALL DATA MODULES FOR THE VIEWS
-    @ObservedObject var userVM = UserViewModel()
-    @ObservedObject var settingsVM = SettingsViewModel()
-    @ObservedObject var mediaVM = MediaViewModel()
-    @ObservedObject var orderVM = OrderViewModel()
-    @ObservedObject var remarksVM = RemarksViewModel()
+    @StateObject var userVM = UserViewModel()
+    @StateObject var settingsVM = SettingsViewModel()
+    @StateObject var mediaVM = MediaViewModel()
+    @StateObject var orderVM = OrderViewModel()
+    @StateObject var remarksVM = RemarksViewModel()
     
     @State var value : CGFloat = -30
     
+    var body: some View {
+        StartViewWrapper()
+    }
+}
+
+struct StartViewWrapper: View {
     var body: some View {
         Group {
             if !settingsVM.hasSettedUp {
@@ -31,10 +36,7 @@ struct AppStart: View {
             } else {
                 UserLogin(userVM: self.userVM, mediaVM: self.mediaVM, orderVM: self.orderVM)
             }
-        }
-        .onAppear {
-            self.settingsVM.getJSON()
-            
+        }.onAppear {
             self.remarksVM.getJSON(session: self.settingsVM.ip, username: self.settingsVM.serverUsername, password: self.settingsVM.serverPassword)
         }
     }
---
+++ Segler/View/Extensions/SeglerColors.swift
@@ -0,0 +1,15 @@
+//
+//  SeglerColors.swift
+//  Segler
+//
+//  Created by Maximilian Gravemeyer on 26.11.20.
+//  Copyright © 2020 Maximilian Gravemeyer. All rights reserved.
+//
+
+import Foundation
+
+extension Color {
+    public static var myCustomColor: Color {
+        return Color(UIColor(red: 219/255, green: 175/255, blue: 15/255, alpha: 1.0))
+    }
+}
