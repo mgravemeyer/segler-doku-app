79f55bdef90f95f083f26008ebc9dd1fc53071b0
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-07 22:07:31 +0200

    updated pdf failed to save error message

--- Segler/View/PDFViews/PDFDetailUIView.swift
+++ Segler/View/PDFViews/PDFDetailUIView.swift
@@ -6,6 +6,7 @@ struct PDFDetailUIView: UIViewRepresentable {
     init(selectedPDF: PDF) {
         self.selectedPDF = selectedPDF
         pdfView.autoScales = false
+        print("old data: \(selectedPDF.data)")
     }
 
     let pdfView = PDFView()
@@ -25,11 +26,12 @@ struct PDFDetailUIView: UIViewRepresentable {
     func savePDF() {
         guard
             let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first,
-            let data = pdfView.document!.dataRepresentation()
+            let data = pdfView.document?.dataRepresentation()
         else {
-                print("error while finding files")
+                print("error while saving or finding files")
                 return
             }
+        print("new data: \(pdfView.document!.dataRepresentation())")
         let fileURL = url.appendingPathComponent("\(UUID().uuidString).pdf")
         do {
             try data.write(to: fileURL)
