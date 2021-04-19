599a973157c044ea9ba8730a338d27258b758067
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-19 23:02:29 +0200

    updated ui to display archive files

--- Segler/View/PDFViews/PDFListView.swift
+++ Segler/View/PDFViews/PDFListView.swift
@@ -12,6 +12,14 @@ struct PDFListView: View {
             ForEach(settingsVM.pdfs, id: \.self) {
                 NavigationLink($0.name, destination: PDFListDetailView(settingsVM: self.settingsVM, remarksVM: self.remarksVM, selectedPDF: $0, show: self.$show))
                     .frame(height: 34)
+            }
+            HStack {
+                Image(systemName: "archivebox.fill")
+                Text("Archiv - - - - - - - - - - - - - -")
+            }.foregroundColor(Color.gray)
+            ForEach(settingsVM.archive, id: \.self) {
+                NavigationLink($0.name, destination: PDFListDetailView(settingsVM: self.settingsVM, remarksVM: self.remarksVM, selectedPDF: $0, show: self.$show))
+                    .frame(height: 34).foregroundColor(Color.gray)
             }.navigationTitle("Protokolle").onAppear{
                 if self.show != true {
                     self.presentationMode.wrappedValue.dismiss()
