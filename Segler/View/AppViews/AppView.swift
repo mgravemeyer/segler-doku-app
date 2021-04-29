62707ddc7e13ac5130653de370bacc20032bdd9e
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-29 11:05:50 +0200

    refactored testImageView

--- Segler/View/AppViews/AppView.swift
+++ Segler/View/AppViews/AppView.swift
@@ -279,7 +279,7 @@ struct SectionBilder: View {
                             ForEach((0...mediaVM.highestOrderNumber).reversed(), id:\.self) { i in
                                 ForEach(mediaVM.images, id:\.self) { image in
                                     if image.selected && image.order == i {
-                                        testImageView(imageObject: image,id: image.id)
+                                        ImagePreviewView(imageObject: image,id: image.id)
                                     }
                                 }
                                 ForEach(mediaVM.imagesCamera, id:\.self) { image in
@@ -300,7 +300,7 @@ struct SectionBilder: View {
                             }
                         } else {
                                 ForEach(mediaVM.images, id:\.self) { image in
-                                    testImageView(imageObject: image,id: image.id)
+                                    ImagePreviewView(imageObject: image,id: image.id)
                                 }
                                 ForEach(mediaVM.imagesCamera, id:\.self) { image in
                                     ImageCameraPreviewView(imageObject: image,id: image.id)
@@ -356,47 +356,6 @@ struct testVideoCameraView: View {
         }
     }
 }
-
-    
-    struct testImageView: View {
-        
-        @EnvironmentObject var mediaVM: MediaViewModel
-        @State var imageObject : ImageModel
-        @State var showSheet = false
-        @State var id : UUID
-        
-        var body: some View {
-            Button(action: {
-                self.showSheet = !self.showSheet
-            }) {
-                ZStack {
-                    Rectangle().frame(width: 100, height: 100).background(Color.black).opacity(0.3).zIndex(2)
-                    Text("Foto").fontWeight(.bold).zIndex(1)
-                    Image(uiImage: imageObject.thumbnail).renderingMode(.original).resizable().frame(width: 100, height: 100).scaledToFill().zIndex(0)
-                }
-                .actionSheet(isPresented: self.$showSheet) { () -> ActionSheet in
-                    ActionSheet(title: Text("Anzeigen - Löschen"), buttons: [
-                        ActionSheet.Button.default(Text("Bild anzeigen"), action: {
-                            self.toggleShowImage()
-                        }),
-                        ActionSheet.Button.destructive(Text("Bild löschen"), action: {
-                            self.toggle(id: self.id)
-                        }),
-                        ActionSheet.Button.cancel()
-                    ])
-                }
-            }
-        }
-        func toggleShowImage() {
-            mediaVM.showImage.toggle()
-            mediaVM.selectedImage = UIImage(data: (imageObject.fetchImage()))
-        }
-        func toggle(id: UUID) {
-            if let index = mediaVM.images.firstIndex(where: {$0.id == id}) {
-                mediaVM.images[index].selected.toggle()
-            }
-        }
-    }
 
 struct testVideoView: View {
 
---
+++ Segler/View/AppViews/SectionImageVideo/ImagePreviewView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct ImagePreviewView: View {
+    
+    @EnvironmentObject var mediaVM: MediaViewModel
+    @State var imageObject : ImageModel
+    @State var showSheet = false
+    @State var id : UUID
+    
+    var body: some View {
+        Button(action: {
+            self.showSheet = !self.showSheet
+        }) {
+            ZStack {
+                Rectangle().frame(width: 100, height: 100).background(Color.black).opacity(0.3).zIndex(2)
+                Text("Foto").fontWeight(.bold).zIndex(1)
+                Image(uiImage: imageObject.thumbnail).renderingMode(.original).resizable().frame(width: 100, height: 100).scaledToFill().zIndex(0)
+            }
+            .actionSheet(isPresented: self.$showSheet) { () -> ActionSheet in
+                ActionSheet(title: Text("Anzeigen - Löschen"), buttons: [
+                    ActionSheet.Button.default(Text("Bild anzeigen"), action: {
+                        self.toggleShowImage()
+                    }),
+                    ActionSheet.Button.destructive(Text("Bild löschen"), action: {
+                        self.toggle(id: self.id)
+                    }),
+                    ActionSheet.Button.cancel()
+                ])
+            }
+        }
+    }
+    func toggleShowImage() {
+        mediaVM.showImage.toggle()
+        mediaVM.selectedImage = UIImage(data: (imageObject.fetchImage()))
+    }
+    func toggle(id: UUID) {
+        if let index = mediaVM.images.firstIndex(where: {$0.id == id}) {
+            mediaVM.images[index].selected.toggle()
+        }
+    }
+}
