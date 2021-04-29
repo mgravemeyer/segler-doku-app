b4149c40ba04e89f2fd29ec2ead82373546df360
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-29 11:18:34 +0200

    refactored ImageView

--- Segler/View/AppViews/AppView.swift
+++ Segler/View/AppViews/AppView.swift
@@ -316,28 +316,3 @@ struct SectionBilder: View {
             }.padding(.horizontal, -15).listRowBackground(self.mediaVM.imagesIsOk ? colors.correctRowColor : colors.warningRowColor)
     }
 }
-
-struct ImageView: View {
-    
-    @EnvironmentObject var mediaVM : MediaViewModel
-    @State var index: Int
-    @State var showSheet = false
-    
-    var body: some View {
-        Button(action: {
-            self.showSheet = !self.showSheet
-        }) {
-            Image(uiImage: self.mediaVM.images[self.index].thumbnail).renderingMode(.original).scaledToFit().frame(width: 120, height: 120)
-            .actionSheet(isPresented: self.$showSheet) { () -> ActionSheet in
-                    ActionSheet(title: Text("Bild löschen"), message: Text("Wirklich Bild löschen?"), buttons: [
-                        ActionSheet.Button.default(Text("Ja"), action: {
-                        }),
-                        ActionSheet.Button.cancel()
-                    ])
-            }
-        }
-    }
-    func delete(at index: Int) {
-        self.mediaVM.images.remove(at: index)
-    }
-}
