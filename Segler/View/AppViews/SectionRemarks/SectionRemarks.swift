1bc17db9320ea9f8d2c08afc9cc750088be602ff
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-29 10:53:54 +0200

    refactored ListCommentsDetailView

--- Segler/View/AppViews/SectionRemarks/SectionRemarks.swift
+++ Segler/View/AppViews/SectionRemarks/SectionRemarks.swift
@@ -55,7 +55,7 @@ struct ListCommentsView: View {
 
             List {
                 ForEach(0..<self.remarksVM.comments.count, id: \.self) { x in
-                    NavigationLink(destination: listCommentsDetail(selection: x, show: self.$show)) {
+                    NavigationLink(destination: ListCommentsDetailView(selection: x, show: self.$show)) {
                         Text("\(self.remarksVM.comments[x].title)")
                             .frame(height: 34)
                     }
@@ -73,5 +73,31 @@ struct ListCommentsView: View {
             }.onAppear {
                 
         }
+    }
+}
+
+struct ListCommentsDetailView: View {
+    let selection : Int
+    @EnvironmentObject var settingsVM: SettingsViewModel
+    @EnvironmentObject var remarksVM : RemarksViewModel
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var show : Bool
+    
+    var body: some View {
+        List {
+            ForEach(0..<self.remarksVM.comments[self.selection].comments.count, id: \.self) { x in
+                Button(action: {
+                    self.remarksVM.selectedComment = self.remarksVM.comments[self.selection].comments[x]
+                    self.settingsVM.savedPDF = PDF(name: "", data: Data())
+                    self.show = false
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("\(self.remarksVM.comments[self.selection].comments[x])")
+                        .frame(height: 34)
+                }
+            }.onAppear {
+                self.remarksVM.bereich = self.remarksVM.comments[self.selection].title
+            }
+        }.navigationBarTitle("Kommentare", displayMode: .inline)
     }
 }
