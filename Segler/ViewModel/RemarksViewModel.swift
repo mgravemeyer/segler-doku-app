9e112c26a59d77af8b93fda0f0fc301215e0f7a5
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-29 19:53:17 +0200

    updated remarksVM json load func

--- Segler/ViewModel/RemarksViewModel.swift
+++ Segler/ViewModel/RemarksViewModel.swift
@@ -13,37 +13,17 @@ class RemarksViewModel: ObservableObject {
     @Published var configLoaded = false
     @Published var commentIsOk = true
     
-    func decoda(jsonData : Foundation.Data) {
+    func loadJSON() {
+        decoderComments(jsonData: NetworkDataManager.shared.config!)
+    }
+    
+    func decoderComments(jsonData : Foundation.Data) {
         let decoder = JSONDecoder()
         do {
             let tempComments = try decoder.decode(ResponseComments.self, from: jsonData)
             comments = tempComments.comments
-//            self.comments = [try decoder.decode(Comment.self, from: jsonData)]
         } catch {
             print("KOMMENTARE KONNTEN NICHT GELADEN WERDEN")
         }
     }
-    
-    func getJSON(session: String, username: String, password: String) {
-        let session = NMSSHSession.init(host: "\(session)", andUsername: "\(username)")
-        session.connect()
-        if session.isConnected{
-            session.authenticate(byPassword: "\(password)")
-            if session.isAuthorized {
-                let sftpsession = NMSFTP(session : session)
-                sftpsession.connect()
-                let content = sftpsession.contents(atPath: "config/config.json")
-                if content != nil {
-                    if let string = String(bytes: content!, encoding: .utf8) {
-                    let jsonData = Foundation.Data(string.data(using: .utf8)!)
-                        decoda(jsonData: jsonData)
-                    }
-                    configLoaded = true
-                } else {
-                    configLoaded = false
-                }
-            }
-        }
-    }
-    
 }
