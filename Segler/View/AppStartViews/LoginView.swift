2175abd4604c4f7063c31127665a8894032fbd67
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-29 09:46:30 +0200

    cleaned up LoginView

--- Segler/View/AppStartViews/LoginView.swift
+++ Segler/View/AppStartViews/LoginView.swift
@@ -19,21 +19,17 @@ struct UserLogin: View {
                 .aspectRatio(contentMode: ContentMode.fit)
                 .frame(width: 280.0)
                 .padding(Edge.Set.bottom, 20)
-//                Button(action: {
-//                    self.userVM.loggedIn = true
-//                }) {
-//                    Text("Ad")
-//                }
-                    Button(action: {
-                        self.mediaVM.loginShowImageScannner = true
-//                      self.userVM.loggedIn = true
-                    }) {
-                        HStack(alignment: .center) {
-                            Spacer()
-                            Text("Anmelden").foregroundColor(Color.white).bold()
-                            Spacer()
-                        }
-                    }.padding().background(Color.green).cornerRadius(4.0)
+                
+                Button(action: {
+                    self.mediaVM.loginShowImageScannner = true
+
+                }) {
+                    HStack(alignment: .center) {
+                        Spacer()
+                        Text("Anmelden").foregroundColor(Color.white).bold()
+                        Spacer()
+                    }
+                }.padding().background(Color.green).cornerRadius(4.0)
                 }.padding().zIndex(0).offset(y: -50)
             if self.mediaVM.loginShowImageScannner {
                 BarcodeScannerSegler(sourceType: 1).zIndex(1)
@@ -41,21 +37,3 @@ struct UserLogin: View {
         }
     }
 }
-
-
-//        Text("Anmelden").bold().font(.title)
-//        TextField("Username", text: $userVM.username)
-//            .padding()
-//            .background(Color(red: 241/255, green: 241/255, blue: 241/255))
-//            .cornerRadius(4.0)
-//            .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-//        SecureField("Passwort", text: $userVM.password)
-//            .padding()
-//            .background(Color(red: 241/255, green: 241/255, blue: 241/255))
-//            .cornerRadius(4.0)
-//            .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-
-                    //DEVICE ID
-//                    Text("\(UIDevice.current.identifierForVendor!)")
-//                    .font(.subheadline)
-//                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 70, trailing: 0))
