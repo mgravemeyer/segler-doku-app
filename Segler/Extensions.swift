61da554b77ad01728fbc539947f900ae30ce86bb
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-29 09:34:38 +0200

    created color extensions

---
+++ Segler/Extensions.swift
@@ -0,0 +1,10 @@
+import UIKit
+import SwiftUI
+
+extension UIColor {
+  static let seglerRed = UIColor(red: 210.0/255.0, green: 105.0/255.0, blue: 130.0/255.0, alpha: 1.0)
+}
+
+extension Color {
+    static let seglerRed = Color(red: 210.0/255.0, green: 105.0/255.0, blue: 130.0/255.0)
+}
