1db46337d25218ba54b2762edf2587db6d287ccb
mgravemeyer <m.gravemeyer@icloud.com>
2021-05-01 10:04:59 +0200

    refactored AppView

--- Segler/View/AppViews/AppView.swift
+++ Segler/View/AppViews/AppView.swift
@@ -16,107 +16,109 @@ struct AppView: View {
     @State var showBarcodeScannerView = false
     
     var body: some View {
-            NavigationView {
-                ZStack {
+            ZStack {
+                NavigationView {
                     ZStack {
-                        List {
-                            SectionOrder(showBarcodeScannerView: self.$showBarcodeScannerView)
-                                .frame(height: 34)
-                            SectionRemarks()
-                                .frame(height: 34)
-                            SectionFreeTextField()
-                            SectionImageViewView()
-                            if !(settingsVM.errorsJSON.isEmpty) {
-                                Image("Warning").resizable().frame(width: 50, height: 50)
-                                ForEach(settingsVM.errorsJSON, id: \.self) { error in
-                                    Text(error).foregroundColor(Color.red)
+                        ZStack {
+                            List {
+                                SectionOrder(showBarcodeScannerView: self.$showBarcodeScannerView)
+                                    .frame(height: 34)
+                                SectionRemarks()
+                                    .frame(height: 34)
+                                SectionFreeTextField()
+                                SectionImageViewView()
+                                if !(settingsVM.errorsJSON.isEmpty) {
+                                    Image("Warning").resizable().frame(width: 50, height: 50)
+                                    ForEach(settingsVM.errorsJSON, id: \.self) { error in
+                                        Text(error).foregroundColor(Color.red)
+                                    }
                                 }
                             }
+                                .zIndex(0)
+                                .environment(\.defaultMinListRowHeight, 8)
+                            VStack {
+                                Spacer()
+                                SaveButtonView()
+                                    .zIndex(2)
+                            }
+                            if mediaVM.showVideo {
+                                VideoDetail()
+                                    .zIndex(1)
+                                    .onAppear {
+                                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                    }
+                            }
+                            if mediaVM.showImage {
+                                PhotoDetail()
+                                    .zIndex(1)
+                                    .onAppear {
+                                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                    }
+                            }
                         }
-                            .zIndex(0)
-                            .environment(\.defaultMinListRowHeight, 8)
-                        VStack {
-                            Spacer()
-                            SaveButtonView()
-                                .zIndex(2)
+                    .navigationBarItems(leading:(
+                    HStack {
+                        NavigationLink(destination: Settings_View()) {
+                            Image(systemName: "gear").font(.system(size: 25))
                         }
-                        if mediaVM.showVideo {
-                            VideoDetail()
-                                .zIndex(1)
-                                .onAppear {
-                                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                                }
-                        }
-                        if mediaVM.showImage {
-                            PhotoDetail()
-                                .zIndex(1)
-                                .onAppear {
-                                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                                }
+                        if !settingsVM.useFixedUser {
+                            NavigationLink(destination: Webview(url: "\(settingsVM.helpURL)").navigationBarTitle("Hilfe")) {
+                                Image(systemName: "questionmark.circle").font(.system(size: 25))
+                            }
                         }
                     }
-                .navigationBarItems(leading:(
-                HStack {
-                    NavigationLink(destination: Settings_View()) {
-                        Image(systemName: "gear").font(.system(size: 25))
-                    }
-                    if !settingsVM.useFixedUser {
-                        NavigationLink(destination: Webview(url: "\(settingsVM.helpURL)").navigationBarTitle("Hilfe")) {
-                            Image(systemName: "questionmark.circle").font(.system(size: 25))
-                        }
-                    }
-                }
-                ), trailing:
-                    (
-                HStack {
-                    Button(action: {
-                    self.userVM.username = ""
-                    self.userVM.loggedIn = false
-                    self.orderVM.machineName = ""
-                    self.orderVM.orderNr = ""
-                    self.orderVM.orderPosition = ""
-                    self.mediaVM.images.removeAll()
-                    self.remarksVM.selectedComment = ""
-                    self.remarksVM.additionalComment = ""
-                    self.orderVM.orderNrIsOk = true
-                    self.remarksVM.commentIsOk = true
-                    self.mediaVM.imagesIsOk = true
-                    }, label: {
+                    ), trailing:
+                        (
+                    HStack {
+                        Button(action: {
+                        self.userVM.username = ""
+                        self.userVM.loggedIn = false
+                        self.orderVM.machineName = ""
+                        self.orderVM.orderNr = ""
+                        self.orderVM.orderPosition = ""
+                        self.mediaVM.images.removeAll()
+                        self.remarksVM.selectedComment = ""
+                        self.remarksVM.additionalComment = ""
+                        self.orderVM.orderNrIsOk = true
+                        self.remarksVM.commentIsOk = true
+                        self.mediaVM.imagesIsOk = true
+                        }, label: {
+                            if settingsVM.useFixedUser {
+                                Text("")
+                            } else {
+                                Image(systemName: "xmark").font(.system(size: 25))
+                            }
+                        })
                         if settingsVM.useFixedUser {
-                            Text("")
-                        } else {
-                            Image(systemName: "xmark").font(.system(size: 25))
-                        }
-                    })
-                    if settingsVM.useFixedUser {
-                        NavigationLink(destination: Webview(url: "\(settingsVM.helpURL)").navigationBarTitle("Hilfe")) {
-                            Image(systemName: "questionmark.circle").font(.system(size: 25))
+                            NavigationLink(destination: Webview(url: "\(settingsVM.helpURL)").navigationBarTitle("Hilfe")) {
+                                Image(systemName: "questionmark.circle").font(.system(size: 25))
+                            }
                         }
                     }
+                    )
+                    ).navigationBarTitle(settingsVM.useFixedUser ? Text("\(settingsVM.userUsername)") : Text("\(userVM.username)"), displayMode: .inline)
                 }
-                )
-                ).navigationBarTitle(settingsVM.useFixedUser ? Text("\(settingsVM.userUsername)") : Text("\(userVM.username)"), displayMode: .inline)
             }
-        }
-        .sheet(isPresented: self.$mediaVM.showImagePickerNew) {
-            ImageSelectionModal()
+            .sheet(isPresented: self.$mediaVM.showImagePickerNew) {
+                ImageSelectionModal()
+                .onAppear {
+                    mediaVM.fetchMedia()
+                }
+            }
             .onAppear {
-                mediaVM.fetchMedia()
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
+                    self.keyboardIsShown = true
+                }
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (noti) in
+                    self.keyboardIsShown = false
+                }
+            }.accentColor(Color.white).navigationViewStyle(StackNavigationViewStyle())
+            if self.mediaVM.showImagePicker {
+                MediaPickerView()
             }
-        }
-        .onAppear {
-            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
-                self.keyboardIsShown = true
+            if self.showBarcodeScannerView {
+                BarcodeScannerView(showBarcodeScannerView: self.$showBarcodeScannerView, sourceType: 1)
             }
-            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (noti) in
-                self.keyboardIsShown = false
-            }
-        }.accentColor(Color.white).navigationViewStyle(StackNavigationViewStyle())
-        if self.mediaVM.showImagePicker {
-            MediaPickerView()
-        }
-        if self.showBarcodeScannerView {
-            BarcodeScannerView(showBarcodeScannerView: self.$showBarcodeScannerView, sourceType: 1)
         }
     }
 }
