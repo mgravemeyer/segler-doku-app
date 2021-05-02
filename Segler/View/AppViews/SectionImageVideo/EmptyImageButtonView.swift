02afabe4f3a4fd27687844530d2e4c12ea029093
mgravemeyer <m.gravemeyer@icloud.com>
2021-05-02 15:57:19 +0200

    refactored EmptyImageButtonView

--- Segler/View/AppViews/SectionImageVideo/EmptyImageButtonView.swift
+++ Segler/View/AppViews/SectionImageVideo/EmptyImageButtonView.swift
@@ -10,52 +10,27 @@ struct EmptyImageButtonView: View {
     
     var body: some View {
         Group {
-            if !UIDevice.current.name.contains("iPod touch") {
-                Button(action: {
-                    UIApplication.shared.endEditing()
-                    self.showSheet = !self.showSheet
-                }) {
-                    
-
-                    ZStack {
-                        Image("Camera")
-                            .resizable()
-                            .renderingMode(.template)
-                            .foregroundColor(color.color)
-                            .frame(width: 40, height: 40)
-                            .zIndex(1)
-                        RoundedRectangle(cornerRadius: CGFloat(3))
-                            .foregroundColor(Color.clear)
-                            .frame(width: 40, height: 145)
-                            .zIndex(0)
-                    }
-                }
-            } else {
-                Button(action: {
-                    UIApplication.shared.endEditing()
-                    self.mediaVM.sourceType = 0
-                    self.mediaVM.showImagePicker = !self.mediaVM.showImagePicker
-                }) {
-                    ZStack {
-                        Image("Camera")
-                            .resizable()
-                            .renderingMode(.template)
-                            .foregroundColor(color.color)
-                            .frame(width: 40, height: 40)
-                            .zIndex(1)
-                        RoundedRectangle(cornerRadius: CGFloat(3))
-                            .foregroundColor(Color.clear)
-                            .frame(width: 40, height: 145)
-                            .zIndex(0)
-                    }
+            Button(action: {
+                UIApplication.shared.endEditing()
+                self.showSheet = !self.showSheet
+            }) {
+                ZStack {
+                    Image("Camera")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(color.color)
+                        .frame(width: 40, height: 40)
+                        .zIndex(1)
+                    RoundedRectangle(cornerRadius: CGFloat(3))
+                        .foregroundColor(Color.clear)
+                        .frame(width: 40, height: 145)
+                        .zIndex(0)
                 }
             }
         }
-
             .actionSheet(isPresented: self.$showSheet) { () -> ActionSheet in
                 ActionSheet(title: Text("Bild/Video hinzufügen"), message: Text("Kamera oder Galerie auswählen"), buttons: [
                     ActionSheet.Button.default(Text("Kamera"), action: {
-                        self.mediaVM.sourceType = 0
                         self.mediaVM.showImagePicker = !self.mediaVM.showImagePicker
                     }),
                     ActionSheet.Button.default(Text("Galerie"), action: {
